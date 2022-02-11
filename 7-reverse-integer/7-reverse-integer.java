0702cb7d071cfefc1baf41b5a767a89278d349a4
sollip <sollip4293@hanmail.net>
2022-02-11 13:08:12 +0900

    Time: 3 ms (33.31%), Space: 41.4 MB (10.86%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+ public int reverse(int x) {
+         StringBuilder result = new StringBuilder();
+        if (x < 0) {
+            result.append("-");
+        }
+        char[] numbers = String.valueOf(x).toCharArray();
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (numbers[i] != '-') {
+                result.append(numbers[i]);
+            }
+        }
+
+        long num = Long.valueOf(String.valueOf(result));
+        if (num > Integer.MAX_VALUE || num < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) num;
+
+    }
+}
